aad08f607684b2bf5b83f81dafb82d2eff1448c9
nataszczypiora <iga423@gmail.com>
2020-11-05 16:50:36 +0100

    Zadanie 2 (prawdopodobnie) GA

--- SN_Igi/index.js
+++ SN_Igi/index.js
@@ -3,6 +3,8 @@ var perceptrons
 var currentSize
 var console = document.getElementById("outputConsole")
 let n = 0.2137
+let xMin = 0.0
+let xMax = 1.0
 
 let digits = [
     [
@@ -59,21 +61,21 @@ let digits = [
         [0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0],
         [0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0],
         [0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0],
-        [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]    ],
+        [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
     [
         [0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
         [0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0],
         [0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
         [0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
         [0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-        [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]    ],
+        [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
     [
         [0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0],
         [0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0],
         [0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0],
         [0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0],
         [0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0],
-        [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]    ],
+        [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
     [
         [0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0],
         [0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0],
@@ -85,34 +87,36 @@ let digits = [
 ]
 let furierSize = [32, 64, 64, 64, 128, 128]
 
-
-function module(a, b){
-    return Math.sqrt((a*a)+(b*b))
+function normalize(x) {
+    return parseFloat((x - xMin) / (xMax - xMin))
 }
 
-function fourier( in_array ){
+function module(a, b) {
+    return Math.sqrt((a * a) + (b * b))
+}
+
+function fourier(in_array) {
     var len = in_array.length;
     var output = new Array();
 
-    for( var k=0; k < len; k++ ) {
+    for (var k = 0; k < len; k++) {
         var real = 0;
         var imag = 0;
-        for( var n=0; n < len; n++ ) {
-            real += in_array[n]*Math.cos(-2*Math.PI*k*n/len);
-            imag += in_array[n]*Math.sin(-2*Math.PI*k*n/len);
+        for (var n = 0; n < len; n++) {
+            real += in_array[n] * Math.cos(-2 * Math.PI * k * n / len);
+            imag += in_array[n] * Math.sin(-2 * Math.PI * k * n / len);
         }
-        output.push(module(real, imag))
+        output.push(normalize(module(real, imag)))
     }
     return output;
 }
 
-function addFourierTransform(array){
-    var sourceArray = new Array(furierSize[currentSize-5])
-    for(i = 0; i < furierSize[currentSize-5];i++){
-        if (i<currentSize*currentSize){
+function addFourierTransform(array) {
+    var sourceArray = new Array(furierSize[currentSize - 5])
+    for (i = 0; i < furierSize[currentSize - 5]; i++) {
+        if (i < currentSize * currentSize) {
             sourceArray[i] = array[i]
-        }
-        else{
+        } else {
             sourceArray[i] = 0
         }
     }
@@ -142,39 +146,40 @@ var field = function (x, y, marked, btn) {
     }
 }
 
-var perceptron = function (digit) {
+var perceptron = function (digit, b = 0) {
     this.digit = digit
-
-    this.weights = new Array(currentSize*currentSize)
+    this.bias = b  // Dodaje bias, jeżeli w konstruktosze przekaże się jakąś wartość
+    this.weights = new Array(currentSize * currentSize + furierSize[currentSize - 5])
     this.teta = Math.random()
 
-    this.getTheta = function (){
+    this.getTheta = function () {
         return this.teta
     }
 
-    this.countValue = function(input){
+    this.countValue = function (input) {
         var digitTable = addFourierTransform(input)
         var result = 0.0
-        for(var i = 0; i < currentSize * currentSize + furierSize[currentSize-5]; i++)
-        {
+        for (var i = 0; i < currentSize * currentSize + furierSize[currentSize - 5]; i++) {
             result += parseFloat(digitTable[i] * this.weights[i])
         }
+        result += this.bias * this.weights[currentSize * currentSize + furierSize[currentSize - 5]]
         return parseFloat(result)
     }
 
     this.perceptronLearning = function () {
-        for (var i = 0; i < currentSize * currentSize + furierSize[currentSize - 5]; i++) {
+        for (var i = 0; i <= currentSize * currentSize + furierSize[currentSize - 5]; i++) {
             this.weights[i] = Math.random()
         }
-        for (var k = 0; k < 420; k++){
+        for (var k = 0; k < 420; k++) {
             var random = parseInt(Math.random() * 10)
             var exVal = random == digit ? 1 : 0
-            var exTab = addFourierTransform(digits[random][currentSize-5])
-            var ERROR = exVal - (this.countValue(digits[random][currentSize-5]) < this.teta ?0:1)
+            var exTab = addFourierTransform(digits[random][currentSize - 5])
+            var ERROR = exVal - (this.countValue(digits[random][currentSize - 5]) < this.teta ? 0 : 1) // <- Tu też mam funkcję progową, ale nie ogarnęłam wcześniej
             if (ERROR != 0) {
                 for (var i = 0; i < currentSize * currentSize + furierSize[currentSize - 5]; i++) {
-                    this.weights[i] = this.weights[i]  + n * ERROR * exTab[i]
+                    this.weights[i] = this.weights[i] + n * ERROR * exTab[i]
                 }
+                this.weights[currentSize * currentSize + furierSize[currentSize - 5]] += n * ERROR * this.bias
                 this.teta = this.teta - n * ERROR
             }
         }
@@ -227,7 +232,7 @@ function createGrid(size) {
     mainContainer.appendChild(buttonTable)
     console.textContent = ""
     perceptrons = new Array(10)
-    for (var i= 0; i<10;i++){
+    for (var i = 0; i < 10; i++) {
         perceptrons[i] = new perceptron(i)
         perceptrons[i].perceptronLearning()
     }
@@ -304,22 +309,20 @@ function paintFromTab(tab) {
     }
 }
 
-function recogniseDigit(){
+function recogniseDigit() {
     console.textContent = ""
-    var input = new Array(currentSize*currentSize)
+    var input = new Array(currentSize * currentSize)
     var k = 0
-    for (var i = 0; i < currentSize; i++){
-        for(var j = 0; j < currentSize; j++)
-        {
-            input[k] = grid[i][j].getMarked()=="true"?1:0
+    for (var i = 0; i < currentSize; i++) {
+        for (var j = 0; j < currentSize; j++) {
+            input[k] = grid[i][j].getMarked() == "true" ? 1 : 0
             k++
         }
     }
-    for(var i = 0; i < 10; i++) {
-        if(perceptrons[i].countValue(input) < perceptrons[i].getTheta()){
+    for (var i = 0; i < 10; i++) {
+        if (perceptrons[i].countValue(input) < perceptrons[i].getTheta()) { // <- O tu jest funkcja aktywacji PROGOWA
             console.textContent += "Perceptron " + i + " NIE ROZPOZNAŁ cyfry\n"
-        }
-        else{
+        } else {
             console.textContent += "Perceptron " + i + " ROZPOZNAŁ cyfrę\n"
         }
     }
@@ -327,7 +330,7 @@ function recogniseDigit(){
 
 createGrid(10)
 
-function reverse(){
+function reverse() {
     for (i = 0; i < currentSize; i++) {
         for (j = 0; j < currentSize; j++) {
             grid[i][j].getBtn().click()
@@ -335,7 +338,7 @@ function reverse(){
     }
 }
 
-function toLeft(){
+function toLeft() {
     var copy = new Array(currentSize)
 
     for (i = 0; i < currentSize; i++) {
@@ -347,19 +350,17 @@ function toLeft(){
 
     for (i = 0; i < currentSize; i++) {
         for (j = 0; j < currentSize - 1; j++) {
-            if(grid[i][j].getMarked() != copy[i][j + 1])
-            {
+            if (grid[i][j].getMarked() != copy[i][j + 1]) {
                 grid[i][j].getBtn().click()
             }
         }
-        if(copy[i][0] != grid[i][currentSize - 1].getMarked())
-        {
+        if (copy[i][0] != grid[i][currentSize - 1].getMarked()) {
             grid[i][currentSize - 1].getBtn().click()
         }
     }
 }
 
-function toRight(){
+function toRight() {
     var copy = new Array(currentSize)
 
     for (i = 0; i < currentSize; i++) {
@@ -371,19 +372,17 @@ function toRight(){
 
     for (i = 0; i < currentSize; i++) {
         for (j = 1; j < currentSize; j++) {
-            if(grid[i][j].getMarked() != copy[i][j - 1])
-            {
+            if (grid[i][j].getMarked() != copy[i][j - 1]) {
                 grid[i][j].getBtn().click()
             }
         }
-        if(copy[i][currentSize - 1] != grid[i][0].getMarked())
-        {
+        if (copy[i][currentSize - 1] != grid[i][0].getMarked()) {
             grid[i][0].getBtn().click()
         }
     }
 }
 
-function toDown(){
+function toDown() {
     var copy = new Array(currentSize)
 
     for (i = 0; i < currentSize; i++) {
@@ -395,19 +394,17 @@ function toDown(){
 
     for (i = 0; i < currentSize; i++) {
         for (j = 1; j < currentSize; j++) {
-            if(grid[j][i].getMarked() != copy[j - 1][i])
-            {
+            if (grid[j][i].getMarked() != copy[j - 1][i]) {
                 grid[j][i].getBtn().click()
             }
         }
-        if(grid[0][i].getMarked() != copy[currentSize - 1][i])
-        {
+        if (grid[0][i].getMarked() != copy[currentSize - 1][i]) {
             grid[0][i].getBtn().click()
         }
     }
 }
 
-function toUp(){
+function toUp() {
     var copy = new Array(currentSize)
 
     for (i = 0; i < currentSize; i++) {
@@ -419,13 +416,11 @@ function toUp(){
 
     for (i = 0; i < currentSize; i++) {
         for (j = 1; j < currentSize; j++) {
-            if(grid[j - 1][i].getMarked() != copy[j][i])
-            {
+            if (grid[j - 1][i].getMarked() != copy[j][i]) {
                 grid[j - 1][i].getBtn().click()
             }
         }
-        if(grid[currentSize - 1][i].getMarked() != copy[0][i])
-        {
+        if (grid[currentSize - 1][i].getMarked() != copy[0][i]) {
             grid[currentSize - 1][i].getBtn().click()
         }
     }
